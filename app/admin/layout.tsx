30783f8bdaf648163d4e54a07ff97a2d11a2253a
leo-z <comeback3278@gmail.com>
2026-09-14 22:33:56 +0900

    feat: change header

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,7 @@ export default function AdminLayout({children} :
         <div className="flex min-h-screen bg-background">
             {/* sidebar */}
             <aside className="w-64 border-r bg-card">
-                <div className="flex h-16 items-center border-b px-6">
+                <div className="flex h-24 items-center border-b px-6">
                     <h1 className="text-lg font-bold">
                         Admin Dashboard
                     </h1>
@@ -12,7 +12,7 @@ export default function AdminLayout({children} :
                 {/* navigation */}
                 <nav className="p-4">
                     <div className="space-y-2">
-                        <a href="admin" className="block rouded-md p-4 text-sm font-medium hover:bg-muted">
+                        <a href="admin" className="block rounded-md p-4 text-sm font-medium hover:bg-muted">
                             Dashboard
                         </a>
                         <a href="admin/orders" className="block rouded-md p-4 text-sm font-medium hover:bg-muted">
@@ -31,18 +31,44 @@ export default function AdminLayout({children} :
                 </nav>
             </aside>
             {/*main*/}
-            <main className="flex-1">
-                <header className="h-16 border-b">
-                    <div className="flex h-full items-center px-6">
-                        <p className="text-lg font-semibold">
-                            Title
+            <main className="flex-1 bg-stone-50">
+                <header className="flex flex-col h-24 border-b text-sm">
+                    {/* route search logout */}
+                    <div className="flex w-full items-center">
+                        {/* route */}
+                        <p className="px-4">
+                            Dashboard  ＞  Products  ＞  All Products 
                         </p>
+                        {/* search logout */}
                         <div className="flex flex-1 items-center justify-end gap-4">
-                            <span className="text-sm text-gray-500">
-                                Dashboard
+                            {/* search */}
+                            <span className="text-md">
+                                Search...
                             </span>
+                            {/* logout */}
                             <button className="rounded-md border p-2">
                                 logout
+                            </button>
+                        </div>
+                    </div>
+                    {/* tab export addProducts */}
+                    <div className="flex items-center py-2">
+                        {/* tab */}
+                        <div className="flex bg-stone-200 ml-4">
+                            <button className="p-2">All</button>
+                            <button className="p-2">Active</button>
+                            <button className="p-2">Draft</button>
+                            <button className="p-2">Archive</button>   
+                        </div>
+                        {/* export addProducts */}
+                        <div className="flex flex-1 items-center justify-end gap-4">
+                            {/* export */}
+                            <button className="rounded-sm border p-2 bg-gray-100">
+                                Export
+                            </button>
+                            {/* add products */}
+                            <button className="rounded-md border p-2 bg-black text-white">
+                                add products
                             </button>
                         </div>
                     </div>
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,11 @@
 export default function AdminPage() {
     return (
         <div>
-            <h1 className="text-2xl">
-                Dashboard
+            <h1 className="text-2xl font-semibolt">
+                Products
             </h1>
-            <p className="mt-2">
-                Welcome to admin dashboad.
+            <p className="mt-2 text-sm text-gray-500">
+                Manage your products and view their sales performance.
             </p>
         </div>
     )
